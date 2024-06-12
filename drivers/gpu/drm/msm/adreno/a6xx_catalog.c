31a7f8d4c2736d310e88e40bbbf1632c3b018c12
N1kroks <nikroksm@mail.ru>
2024-06-12 11:30:13 +0500

    drm/msm/adreno: Add A618 hwcg
    Signed-off-by: Vitalii Skorkin <nikroksm@mail.ru>

--- drivers/gpu/drm/msm/adreno/a6xx_catalog.c
+++ drivers/gpu/drm/msm/adreno/a6xx_catalog.c
@@ -773,6 +773,7 @@ static const struct adreno_info a6xx_gpus[] = {
 		.init = a6xx_gpu_init,
 		.zapfw = "a615_zap.mbn",
 		.a6xx = &(const struct a6xx_info) {
+			.hwcg = a615_hwcg,
 			.protect = &a630_protect,
 			.gmu_cgc_mode = 0x00000222,
 			.prim_fifo_threshold = 0x00180000,
